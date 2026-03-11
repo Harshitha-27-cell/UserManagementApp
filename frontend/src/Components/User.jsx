6d1136424ee4cbb859efe5523fac93218d5e29ac
Harshitha-27-cell <kachapuramharshitha@gmail.com>
2026-03-11 22:36:49 +0530

    Updated User.jsx

--- frontend/src/Components/User.jsx
+++ frontend/src/Components/User.jsx
@@ -3,25 +3,22 @@ import {useLocation} from 'react-router-dom'
 export default function User() {
 
   let {state}=useLocation()
-
   let user=state?.user
+
+  if(!user){
+    return <h2 className="p-10 text-lg">No user selected</h2>
+  }
 
   return (
     <div className="p-10">
       <h1 className="text-2xl font-bold mb-4">User Details</h1>
 
-      {
-        user &&
-        <div className="border p-6 rounded shadow w-96">
-
-          <p><b>Name :</b> {user.username}</p>
-          <p><b>Email :</b> {user.email}</p>
-          <p><b>DOB :</b> {new Date(user.dateOfBirth).toLocaleDateString()}</p>
-          <p><b>Mobile :</b> {user.mobileNumber}</p>
-
-        </div>
-      }
-
+      <div className="border p-6 rounded shadow w-96">
+        <p><b>Name :</b> {user.username}</p>
+        <p><b>Email :</b> {user.email}</p>
+        <p><b>DOB :</b> {new Date(user.dateOfBirth).toLocaleDateString()}</p>
+        <p><b>Mobile :</b> {user.mobileNumber}</p>
+      </div>
     </div>
   )
 }
