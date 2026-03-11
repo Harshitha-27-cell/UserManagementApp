c8e88bd4ede94311413f19e936fe6fd85b9af357
Harshitha-27-cell <kachapuramharshitha@gmail.com>
2026-03-11 22:12:01 +0530

    Updated vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base:'/MERNStack_Assignments/',
+  base:'/',
   plugins: [react()],
 })
